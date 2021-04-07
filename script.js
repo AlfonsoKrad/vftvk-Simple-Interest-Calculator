d5877013d425ddb882b2edd10ba81bc57b064dae
AlfonsoKrad <65353103+AlfonsoKrad@users.noreply.github.com>
2021-04-07 13:35:43 -0500

    IBM Proyect

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     
+    if( principal <= 0)
+    {
+        alert("Enter a positive number")
+        document.getElementById("principal").focus();
+        return;
+    }
+    else
+    {
+        var interes = principal*years*rate/100;
+        var futureYear = parseInt(new Date().getFullYear()) +parseInt(years);
+        
+        // Concatenate the values needed in order to sett to the result element
+        var newPara ="If you deposit " + "<mark>" + principal + "</mark>" + "," + '<br>';
+        var newText = "at an interest rate of " + "<mark>" + rate + "%" + "</mark>"+ "," + '<br>';
+        var newText2 = "You will receive an amount of " + "<mark>" + interes + "</mark>" + "," + '<br>' ;
+        var newText3 = "in the year " + "<mark>" + futureYear + "</mark>";
+    
+        newPara = newPara + newText + newText2 +newText3;
+        document.getElementById("result").innerHTML = newPara;
+    }
+}
+
+function currentRangeValue()
+{
+    // Set values to rateValues element
+    var rateValue = document.getElementById("rate").value;
+    document.getElementById("rateValue").innerHTML = rateValue+" %";
 }
         
